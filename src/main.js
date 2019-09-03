ab5a358a512a24838a390cd712366dbed9cbe722
wmss-wj <wmss_wj@qq.com>
2019-09-03 17:19:12 +0800

    计算加载app.js的时长，使用启动界面的方式掩盖启动白屏的可能性

--- src/main.js
+++ src/main.js
@@ -5,8 +5,18 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let end = new Date().getTime()
+let loading = 1500
+if (end - window.myStartTime > loading) {
+  loading = 1
+} else {
+  loading = loading - (end - window.myStartTime)
+}
+
+setTimeout(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+}, loading)
